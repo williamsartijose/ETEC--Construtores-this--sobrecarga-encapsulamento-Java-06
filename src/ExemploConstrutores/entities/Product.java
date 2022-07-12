82996f2ee99582fa0ebf36a852aa90966418f1ec
William sarti josé <william.jose@totvs.com.br>
2022-07-11 23:09:26 -0300

    Palavra SOBRECARGA

--- src/ExemploConstrutores/entities/Product.java
+++ src/ExemploConstrutores/entities/Product.java
@@ -12,6 +12,17 @@ public class Product {
 		this.quantity = quantity;
 	}
 	
+	
+//	Proposta de melhoria
+//	• Vamos criar um construtor opcional, o qual recebe apenas nome e
+//	preço do produto. A quantidade em estoque deste novo produto, por
+//	padrão, deverá então ser iniciada com o valor zero. • Nota: é possível também 
+//	incluir um construtor padrão
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
 
 	public double totalValueInStock() {
 		return price * quantity;
